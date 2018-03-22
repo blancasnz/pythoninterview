1d84ee2ba542d87f5554c43a390898a295fde02f
Manuel Sanchez <manuelsanchez1100@gmail.com>
2018-03-21 23:06:00 -0700

    Fancier rotate matrix

--- rotate_matrix.py
+++ rotate_matrix.py
@@ -14,3 +14,24 @@ def rotate_matrix(matrix):
             matrix[dimension - 1 - layer - i][layer] = bottom
 
     return matrix
+
+def rotate_matrix_v2(matrix):
+    """
+    1 2 3
+    4 5 6
+    7 8 9
+
+    reversed
+    7 8 9
+    4 5 6
+    1 2 3
+
+    zipped
+    (7, 4 1), (8, 5, 2), (9, 6, 3)
+
+    list(zipped)
+    7 4 1
+    8 5 2
+    9 6 3
+    """
+    return list(zip(*reversed(matrix)))
