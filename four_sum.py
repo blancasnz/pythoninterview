a44a8dca84f3ad27ad1748190c64189d55af298e
Manuel Sanchez <manuelsanchez1100@gmail.com>
2018-08-28 20:13:09 -0700

    Four sum II

---
+++ four_sum.py
@@ -0,0 +1,19 @@
+
+
+def four_sum(A, B, C, D):
+
+    sums = {}
+
+    for a in A:
+        for b in B:
+            if a + b in sums:
+                sums[a+b] += 1
+            else:
+                sums[a+b] = 1
+
+    counts = 0
+    for c in C:
+        for d in D:
+            if -c - d in sums:
+                counts += 1
+    return counts
